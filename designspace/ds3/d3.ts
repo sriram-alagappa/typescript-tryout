8b928b0e7919fd4522d576152bf539b8f373554c
sriram <sree6644@gmail.com>
2015-12-05 08:59:43 -0700

    some tryout

--- designspace/ds3/d3.ts
+++ designspace/ds3/d3.ts
@@ -2,7 +2,8 @@ module ds3{
 	angular.module("ds3",["ui.router"]);
 	
 	angular.module('ds3').config(function($stateProvider,$urlRouterProvider){
-		$urlRouterProvider.otherwise("/landing");
+		$urlRouterProvider.otherwise("/");
+		$stateProvider.state("home",{url:"/",templateUrl:"ds3/welcome/welcome-view.html"})
 		$stateProvider.state("landing",{url:"/landing",templateUrl:"ds3/landing/landingview.html",controller:"LandingViewCtrl as vm"})
 	});
 	
